9d55fbee8d9c143e42381f828588c322dde4edc4
Andrey Hoffmann <andrey.hoffmann@outlook.com.br>
2015-03-11 17:54:10 -0300

    Update sass, anbd html files

--- www/app/app.js
+++ www/app/app.js
@@ -1,1 +1,18 @@
- var module = angular.module('app', ['onsen']);
+ 'use strict';
+ var app = null;
+ (function () {
+     app = angular.module('app', ['onsen']);
+     var runModule = {}
+     runModule.$inject = ['$rootScope'];
+
+     runModule = function ($rootScope) {
+         $rootScope.loadPages = function (navigation) {
+             navigation.pushPage('app/views/first/first.html')
+         }
+
+
+     };
+
+     app.run(runModule);
+
+ }());
